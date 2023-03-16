a3ddb26023fa1f2a8ffb3a625456c29954aea274
lize.live <git@lize.live>
2023-03-16 02:27:01 +0000

    codeserver notes

---
+++ code-server.sh
@@ -0,0 +1,9 @@
+wget -O- https://aka.ms/install-vscode-server/setup.sh | sh
+code-server serve-local --accept-server-license-terms --without-connection-token --server-data-dir=/mnt/cie/code-server
+
+mutantdino.resourcemonitor
+ms-vscode.remote-repositories
+ms-python.python
+VisualStudioExptTeam.vscodeintellicode
+
+
