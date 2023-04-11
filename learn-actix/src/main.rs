db56b5a892774401e2d233efb8a295be618318ba
lize.live <git@lize.live>
2023-04-11 19:40:13 +0000

    schema

--- learn-actix/src/main.rs
+++ learn-actix/src/main.rs
@@ -1,22 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use serde_json::Value;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use schema::base::*;
+    let json = include_str!("../../example/gpu.json");
+
+    let dev_container: DevContainer = serde_json::from_str(json)?;
+
+    println!("{:#?}", dev_container);
+    Ok(())
 }
 
 
 
-mod schema{
-    mod feature{
+pub mod schema{
+    // mod feature{
+    //     use serde::{Deserialize, Serialize};
+    //     schemafy::schemafy!(
+    //         root: DevContainerFeature
+    //         "../schema/devContainerFeature.schema.json"
+    //     );
+    // }
+    pub mod base{
         use serde::{Deserialize, Serialize};
         schemafy::schemafy!(
-            root: DevContainerFeature
-            "schema/devContainerFeature.schema.json"
-        );
-    }
-    mod base{
-        use serde::{Deserialize, Serialize};
-        schemafy::schemafy!(
-            root: DevContainerBase // Optional name for the root type (if one exists)
-            "schema/devContainer.base.schema.json"
+            root: DevContainer // Optional name for the root type (if one exists)
+            "../schema/devContainer.base.schema.json"
         );
     }
 }
