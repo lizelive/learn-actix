f3aa4c671e9f395d905e277247bf440d4f392251
lize.live <git@lize.live>
2023-04-13 19:20:15 -0700

    learn more about schemify

--- learn-actix/src/main.rs
+++ learn-actix/src/main.rs
@@ -1,30 +1,36 @@
-use serde_json::Value;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    use schema::base::*;
-    let json = include_str!("../../example/gpu.json");
+    // use schema::base::*;
+    let _json = include_str!("../../example/meta.json");
 
-    let dev_container: DevContainer = serde_json::from_str(json)?;
+    // let dev_container: DevContainer = serde_json::from_str(json)?;
 
-    println!("{:#?}", dev_container);
+    let x = HashableValue::Null(());
+    println!("{:#?}", x);
+    
     Ok(())
 }
 
+use serde::{Deserialize, Serialize};
+
+schemafy::schemafy!(
+    root: HashableValue // Optional name for the root type (if one exists)
+    "../schema/hashable-value.json"
+);
 
 
-pub mod schema{
-    // mod feature{
-    //     use serde::{Deserialize, Serialize};
-    //     schemafy::schemafy!(
-    //         root: DevContainerFeature
-    //         "../schema/devContainerFeature.schema.json"
-    //     );
-    // }
-    pub mod base{
-        use serde::{Deserialize, Serialize};
-        schemafy::schemafy!(
-            root: DevContainer // Optional name for the root type (if one exists)
-            "../schema/devContainer.base.schema.json"
-        );
-    }
-}
+// pub mod schema{
+//     // mod feature{
+//     //     use serde::{Deserialize, Serialize};
+//     //     schemafy::schemafy!(
+//     //         root: DevContainerFeature
+//     //         "../schema/devContainerFeature.schema.json"
+//     //     );
+//     // }
+//     pub mod base{
+//         use serde::{Deserialize, Serialize};
+//         schemafy::schemafy!(
+//             root: DevContainer // Optional name for the root type (if one exists)
+//             "../schema/devContainer.base.schema.json"
+//         );
+//     }
+// }
